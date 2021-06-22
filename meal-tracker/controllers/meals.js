d187e371b89208f8b553c4fa8a05698745c571bb
Idahosa Ogieva <ida@Idahosas-MacBook-Air.local>
2021-06-22 01:45:31 -0400

    add index and show pages

--- meal-tracker/controllers/meals.js
+++ meal-tracker/controllers/meals.js
@@ -1,11 +1,44 @@
 // const Meal = require('../models/meal');
+const User = require('../models/user');
 
 module.exports = {
-    new: newMeal
+    new: newMeal,
+    create,
+    index,
+    show
 }
 
-function newMeal (req, res) {
-    console.log(req.user.name);
+function show(req, res){
+    // User.findById(req.user.id, function(err, post){
+    //     post.findById(req.params.id, function(err, meal) {
+    //         res.render('meals/show', {post, meal});
+    //         console.log(meal);
+
+    //     })
+    // })
+    User.findById(req.user.id, function(err, meals){
+        res.render('meals/show', {title: 'All Log', meals, id: req.params.id});
+        console.log(req.params.id);
+    })
+}
+
+function index(req, res){
+    User.findById(req.user.id, function(err, meals){
+        res.render('meals/index', {title: 'All logs', meals});
+    })
+}
+
+function create(req, res) {
+    User.findById(req.user.id, function(err, user) {
+        user.post.push(req.body);
+        user.save(function(err) {
+            res.redirect('/meals');
+        })
+    })
+}
+
+function newMeal(req, res) {
+    // console.log(req.user.name);
     res.render('meals/new', {
         details: req.user
     });
--- meal-tracker/routes/meals.js
+++ meal-tracker/routes/meals.js
@@ -5,7 +5,10 @@ const usersCtrl = require('../controllers/users');
 const mealsCtrl = require('../controllers/meals');
 
 
-router.post('/', )
 router.get('/new', mealsCtrl.new);
+router.get('/:id', mealsCtrl.show);
+router.get('/', mealsCtrl.index);
+router.post('/', mealsCtrl.create);
+
 
 module.exports = router;
